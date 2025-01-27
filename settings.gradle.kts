56a9fec2add120440ffcf96aa4af8bb269e84f22
Michael Zuegg <zzuegg@users.noreply.github.com>
2025-01-28 00:49:07 +0100

    :sparkles: feat(gradle): migrate dependency version management to libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,4 @@
 rootProject.name = "mylie.engine"
 
-dependencyResolutionManagement{
-    versionCatalogs{
-        create("libs"){
-            version("lombok","1.18.36")
-            version("logback","1.5.13")
-            version("slf4j","2.0.15")
-
-            library("lombok","org.projectlombok","lombok").versionRef("lombok")
-            library("logging.api","org.slf4j","slf4j-api").versionRef("slf4j")
-            library("logging.runtime","ch.qos.logback","logback-classic").versionRef("logback")
-        }
-    }
-}
-
 include("core")
 project(":core").projectDir=file("engine/core")
